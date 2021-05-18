2748bfaed81bae307f0022fc7aedf34e1a1b2e31
finc8888 <finc8888@gmail.com>
2021-05-18 17:22:17 +0300

    #2Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Lesson from './component/Lesson'
 import reportWebVitals from './reportWebVitals';
+
+const createdElem1 = React.createElement('div', null, 'Simple div with text')
+const createdElem2 = React.createElement('input', {className: 'just-class'});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {createdElem1}
+    {createdElem2}
+      <Lesson/>  
   </React.StrictMode>,
   document.getElementById('root')
 );
